e1bc842fa729a21405b353e252a3e20fdaa2d7dc
github_Pandaaa00 <pandya.cahya99@gmail.com>
2024-02-27 21:56:06 +0700

    Praktikum 2

--- Buku23.java
+++ Buku23.java
@@ -1,12 +1,11 @@
 public class Buku23 {
-    // Atribut
+
     private String judul;
     private String pengarang;
     private int halaman;
     private int stok;
     private int harga;
 
-    // Constructor
     public Buku23(String judul, String pengarang, int halaman, int stok, int harga) {
         this.judul = judul;
         this.pengarang = pengarang;
@@ -15,7 +14,6 @@ public class Buku23 {
         this.harga = harga;
     }
 
-    // Method untuk menampilkan informasi buku
     public void tampilInformasi() {
         System.out.println("Judul: " + judul);
         System.out.println("Pengarang: " + pengarang);
@@ -25,12 +23,10 @@ public class Buku23 {
         System.out.println("===========================");
     }
 
-    // Method untuk menghitung harga total
     public int hitungHargaTotal(int jmlBukuTerjual) {
         return harga * jmlBukuTerjual;
     }
 
-    // Method untuk menghitung diskon
     public double hitungDiskon(int hargaTotal) {
         double diskon = 0;
         if (hargaTotal > 150000) {
@@ -41,25 +37,21 @@ public class Buku23 {
         return diskon;
     }
 
-    // Method untuk menghitung harga bayar setelah diskon
     public double hitungHargaBayar(int hargaTotal) {
         double diskon = hitungDiskon(hargaTotal);
         return hargaTotal - (hargaTotal * diskon);
     }
 
-    // Method untuk menjual buku
     public void terjual(int jml) {
         if (stok > 0) {
             stok -= jml;
         }
     }
 
-    // Method untuk menambah stok buku
     public void restock(int jml) {
         stok += jml;
     }
 
-    // Method untuk mengganti harga buku
     public void gantiHarga(int hrg) {
         harga = hrg;
     }
